dbb7c043b2e77fa4b7f211fa640d194aa6d7bc21
HyyAldi <zheya29@gmail.com>
2026-01-23 15:06:29 +0700

    awal

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan1</title>
+</head>
+
+<body>
+    <!-- <?php
+    // // <!-- variable php -->
+    // $a = 5;
+    // $b = 5;
+    // $c = $a + $b;
+    // // <!-- variable php -->
+    // print "hasil dari $a + $b adalah $c";
+    
+
+
+
+    
+    // // penggabungan string
+    // $nama_depan = "aldi";
+    // $nama_belakang = " kurniawan";
+    // print $nama_depan . $nama_belakang;
+    
+
+    
+    // // assigment
+    // $x = 1;
+    // $x += 2;
+
+    // echo $x;
+
+    // $ndb = "aldi";
+    // $ndb .= " kurniawan";
+
+    // echo $ndb;
+    
+
+    
+    //perbandingan
+    // <, >, <=, >=, ==, !=
+    // var_dump(5 == "5")
+
+    // $a = 11;
+    // $b = "itu benar";
+    // $c = "itu salah";
+
+
+
+    // if ($a < 10) {
+    //     echo $b;
+    // } else {
+    //     echo $c;
+    // }
+
+    //identitas
+    //===, !==
+
+
+    //logika [&&, ||,!] untuk pengkodisian
+    // $a = 50;
+    // var_dump($a < 100 || $a > 10);
+    ?> -->
+    
+    <?php 
+    //PENGULANGAN [for,while,do.. while, foreach = pengulangan khusus array]
+    //selagi nilai nya true maka lakukan terus program yang ada di dalam nya
+
+
+    //for (inisialisasi; kondisi; increment/decrement)
+    // for($i = 0; $i < 10; $i++) {//hanya mencari nilai true
+    // // cara baca nya = untuk(jalankan $1 selama  $1 masih kurang dari 10,
+    // // jika $1 masih kurang dari 10 terus ulang program ini. jika $1 sudah 10 maka program berhenti)
+
+    // print "aldi  <br>";
+
+    // }
+
+    //WHILE
+
+    // $i = 0;
+    // while ($i < 5) {
+    //     echo "hello world <br>";
+
+
+    //     $i++;
+    // }
+
+
+    //DO.. WHILE
+    $i = 0;
+    do {
+    print "hai ";
+    $i++;
+    } while ($i < 5);
+    
+
+    ?>
+
+
+</body>
+
+</html>
