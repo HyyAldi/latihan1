ed9922dc23003c242ff63abed7f2a7b5145125c1
HyyAldi <zheya29@gmail.com>
2026-01-25 17:56:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,125 @@
 
 <body>
     <!-- <?php
-    // // <!-- variable php -->
-    // $a = 5;
-    // $b = 5;
-    // $c = $a + $b;
-    // // <!-- variable php -->
-    // print "hasil dari $a + $b adalah $c";
+            // // <!-- variable php -->
+            // $a = 5;
+            // $b = 5;
+            // $c = $a + $b;
+            // // <!-- variable php -->
+            // print "hasil dari $a + $b adalah $c";
+
+
+
+
+
+            // // penggabungan string
+            // $nama_depan = "aldi";
+            // $nama_belakang = " kurniawan";
+            // print $nama_depan . $nama_belakang;
+
+
+
+            // // assigment
+            // $x = 1;
+            // $x += 2;
+
+            // echo $x;
+
+            // $ndb = "aldi";
+            // $ndb .= " kurniawan";
+
+            // echo $ndb;
+
+
+
+            //perbandingan
+            // <, >, <=, >=, ==, !=
+            // var_dump(5 == "5")
+
+            // $a = 11;
+            // $b = "itu benar";
+            // $c = "itu salah";
+
+
+
+            // if ($a < 10) {
+            //     echo $b;
+            // } else {
+            //     echo $c;
+            // }
+
+            //identitas
+            //===, !==
+
+
+            //logika [&&, ||,!] untuk pengkodisian
+            // $a = 50;
+            // var_dump($a < 100 || $a > 10);
+            ?> -->
+
+    <!-- <?php
+            //PENGULANGAN [for,while,do.. while, foreach = pengulangan khusus array]
+            //selagi nilai nya true maka lakukan terus program yang ada di dalam nya
+
+
+            //for (inisialisasi; kondisi; increment/decrement)
+            // for($i = 0; $i < 10; $i++) {//hanya mencari nilai true
+            // // cara baca nya = untuk(jalankan $1 selama  $1 masih kurang dari 10,
+            // // jika $1 masih kurang dari 10 terus ulang program ini. jika $1 sudah 10 maka program berhenti)
+
+            // print "aldi  <br>";
+
+            // }
+
+            //WHILE
+
+            // $i = 0;
+            // while ($i < 5) {
+            //     echo "hello world <br>";
+
+
+            //     $i++;
+            // }
+
+
+            // DO.. WHILE
+            // $i = 0;
+            // do {
+            // print "Hello ";
+            // $i++;
+            // } while ($i < 5);
+            // 
+            ?> -->
+    <!--  contoh penggunaan PENGULANGAN -->
+    <table border="1" cellpadding="10" cellspacing="0">
+        <!-- <?php
+                for ($i = 1; $i <= 5; $i++) {
+                    echo "<tr>";
+                    for ($j = 1; $j <= 5; $j++) {
+                        echo "<td>$i,$j</td>";
+                    }
+                    echo "</tr>";
+                }
+
+                ?> -->
+
+        <!-- ada cara lain dari yang di atas -->
+
+        <!-- <?php for ($i = 1; $i <= 5; $i++) { ?>
+            <tr>
+                <?php for ($j = 1; $j <= 5; $j++) { ?>
+                    <td><?php echo "$i, $j" ?></td>
+                <?php } ?>
+            </tr>
+        <?php } ?> -->
+
+    </table>
+
+    <!-- PENGKONDISIAN / PERCABANGAN [if else, if else, ternary, switch] -->
     
 
+<<<<<<< Updated upstream
+=======
 
 
     
@@ -91,16 +202,17 @@
 
 
     // DO.. WHILE
-    // $i = 0;
-    // do {
-    // print "hai ";
-    // $i++;
-    // } while ($i < 5);
+    $i = 0;
+    do {
+    print "Hello ";
+    $i++;
+    } while ($i < 5);
     
 
     ?>
 
 
+>>>>>>> Stashed changes
 </body>
 
 </html>
