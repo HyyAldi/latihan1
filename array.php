9372535d0322f38069b04ddd7e900d7638440465
HyyAldi <zheya29@gmail.com>
2026-01-29 20:21:31 +0700

    Update array.php

--- array.php
+++ array.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // ARRAY adalah sebuah variabel yang bisa menampung kebih dari 1 nilai \\
 //ada 2 cara membuat array di php 
 // elemen pada array boleh memiliki tipe data yang berbeda :
@@ -22,14 +22,57 @@
 // echo "<br>";
 // $month[]="juni";
 // print_r($month);
-
-
-
-
-
-
-
-
-
-
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div {
+            height: 100px;
+            width: 100px;
+            border-radius: 10px;
+            background-color: aqua;
+            text-align: center;
+            line-height: 100px;
+            margin: 5px;
+            float: left;
+        }
+
+        ;
+    </style>
+</head>
+
+<body>
+
+    <?php
+    //PENGULANGAN Pada Array
+    // for / 
+    $nilai = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,13];
+    ?>
+
+    <!-- INI STATIC karna ketika $nilai1 di tambah maka dia tidak akan tampil
+     dilayar  -->
+    <!-- <?php for ($i = 0; $i < 10; $i++) : ?>
+    <div><?= $nilai[$i]; ?></div>
+    <?php endfor ?> -->
+
+    <!-- cara dinamis -->
+    <!-- <?php for($i = 0; $i < count($nilai); $i++) :?>
+    <div><?= $nilai[$i]; ?></div>
+    <?php endfor ?> -->
+
+    <!-- Ada yang lebih gampang menggunakan foreach -->
+    <?php foreach( $nilai as $a ) : ?>
+    <div> <?= $a; ?> </div>
+    <?php endforeach ?>
+
+
+</body>
+
+</html>
