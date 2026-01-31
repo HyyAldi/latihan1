5b75cc3f76138aee252284f114f350bb1d5d2d11
HyyAldi <zheya29@gmail.com>
2026-01-31 20:48:34 +0700

    Update array.php

--- array.php
+++ array.php
@@ -33,7 +33,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        div {
+        #div {
             height: 100px;
             width: 100px;
             border-radius: 10px;
@@ -63,16 +63,94 @@
     <?php endfor ?> -->
 
     <!-- cara dinamis -->
-    <!-- <?php for($i = 0; $i < count($nilai); $i++) :?>
+    <!-- <?php for ($i = 0; $i < count($nilai); $i++) : ?>
     <div><?= $nilai[$i]; ?></div>
     <?php endfor ?> -->
 
     <!-- Ada yang lebih gampang menggunakan foreach -->
-    <?php foreach( $nilai as $a ) : ?>
+    <!-- <?php foreach ($nilai as $a) : ?>
     <div> <?= $a; ?> </div>
-    <?php endforeach ?>
+    <?php endforeach ?> -->
 
-    
+    <!-- contoh penggunaan -->
+    <!-- <?php $siswa = ["aldi", "01", "XI TKJ 1"]; ?>
+    <ul>
+        <?php foreach ($siswa as $s) : ?>
+            <li><?= $s ?></li>
+        <?php endforeach ?>
+    </ul> -->
+
+
+    <!-- semisal siswa nya ada lebih dari 1. 
+    menggunakan teknik array dalam array -->
+    <!-- array numerik (not recomend) -->
+    <!-- <?php $murid = [
+                // ini murid pertama
+                ["aldi", "01", "XI TKJ 1"], // harus ada koma 
+                // ini murid kedua
+                ["Lia", "52", "XI idk 1"],
+                //ini murid ketiga
+                ["someone", "25", "XI ipa 2"]
+            ] ?>
+    <ul>
+        <?php foreach ($murid as $mu) : ?>
+            <li>Nama :<?= $mu[0] ?></li>
+            <li>Absen :<?= $mu[1] ?></li>
+            <li>Kelas :<?= $mu[2] ?></li>
+            <br>
+        <?php endforeach ?>
+    </ul> -->
+
+    <!-- Array Associative-->
+    <!-- ini adalah konsep array dalam array -->
+    <!-- [warna ungu] = adalah array di dalam array, [] = array -->
+    <!-- disini ada 1 array yang membungkus 3 array -->
+    <!-- <?php $angka = [ // array 1
+                [1, 2, 3], // array 1.1
+                [4, 5, 6], // array 1.2
+                [7, 8, 9] // array 1.3
+            ] ?> -->
+    <!-- ini adalah cara menampilkannya -->
+    <!-- <?php foreach ($angka as $A) : ?>
+        <?php foreach ($A as $a) : ?>
+            <div style="width: 50px; height: 50px; line-height: 50px;">
+                <?= $a ?>
+            </div>
+        <?php endforeach ?>
+    <?php endforeach ?>
+     -->
+
+
+    <!-- daftar murid menggunakan Array Associative -->
+    <?php $murid = [
+        // ini murid pertama
+        [
+            "nama" => "Aldi",
+            "absen" => "01",
+            "kelas" => "XI TKJ 1"
+        ],
+        // harus ada koma 
+        // ini murid kedua
+        [
+            "nama" => "Lia",
+            "absen" => "52",
+            "kelas" => "XI IDK 1"
+        ],
+        //ini murid ketiga
+        [
+            "nama" => "Someone",
+            "absen" => "25",
+            "kelas" => "XI IPA 1"
+        ],
+    ] ?>
+<?php foreach($murid as $mu) : ?>
+    <li>Nama : <?= $mu["nama"] ?></li>
+    <li>Absen : <?= $mu["absen"] ?></li>
+    <li>Kelas : <?= $mu["kelas"] ?></li>
+    <br>
+<?php endforeach?>
+
+
 
 
 </body>
